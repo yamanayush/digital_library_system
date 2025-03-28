e3b4f4e6f8f3e729d6d44aaba9336147933fc40c
yamanayush <yamanayush27204@gmail.com>
2025-03-29 04:28:04 +0530

    Updated backend logic

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use((err, req, res, next) => {
 // Start Server
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+  console.log(`Server running in ${process.env.NODE_ENV || "production"} mode on port ${PORT}`);
 });
 
 // Handle unhandled promise rejections
